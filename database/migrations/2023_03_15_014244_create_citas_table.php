964958dd1be1a09336517fb9e99a0be55e8885ae
LeninStivenUlcuango <lsulcuangog@utn.edu.ec>
2023-03-14 21:03:44 -0500

    Tabla citas

--- database/migrations/2023_03_15_014244_create_citas_table.php
+++ database/migrations/2023_03_15_014244_create_citas_table.php
@@ -11,11 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id('id_card');
-            $table->string('name');
-            $table->string('email');
-            $table->string('password');
+        Schema::create('citas', function (Blueprint $table) {
+            $table->id();
+            $table->int('user_id');
+            $table->foreing('user_id')->references('id')->on('users');
+            $table->int('service');
+            $table->date();
             $table->timestamps();
         });
     }
@@ -25,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('citas');
     }
 };
